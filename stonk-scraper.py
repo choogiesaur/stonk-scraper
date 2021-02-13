8826c8947a865f00e9ae34ca0c50a4ed4b06bee4
Matthew Henderson <matth397@yahoo.com>
2021-02-12 22:07:09 -0500

    Initial setup of API variables using tweepy

--- stonk-scraper.py
+++ stonk-scraper.py
@@ -1,1 +1,16 @@
+import os
+import tweepy
+import requests
 
+consumer_key 	= os.getenv('TWITTER_API_KEY')
+consumer_secret = os.getenv('TWITTER_API_KEY_SECRET')
+access_token    = os.getenv('TWITTER_ACCESS_TOKEN')
+access_secret   = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
