5308503bab913057bd287ab356f3ea32d4cf439c
Matthew Henderson <matth397@yahoo.com>
2021-02-19 22:44:53 -0500

    Script to extract stock symbols and names from web HTML

---
+++ column_extractor.py
@@ -0,0 +1,16 @@
+# Open scraped stock list
+with open('stonks_100.txt') as f:
+    # Initialize counter variable
+    i = 0
+    # Create string to append stock symbols and stocks
+    output_line = ""
+    for line in f:
+        # Add column 1 to output string
+        if i%12 == 0:
+            output_line = line.strip()
+        # Add column 2 to output string
+        elif i%12 == 1:
+            output_line += "|" + line.strip()
+            print(output_line)
+        # Increment counter
+        i += 1
