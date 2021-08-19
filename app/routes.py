0c97c6e811e0a110006162e3ec9b2768cef15350
Matthew Henderson <info@matthewahenderson.com>
2021-08-19 17:29:10 -0400

    Added to route and HTML files

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,32 @@ def home():
     return render_template(
         'layout.html',
         title="Stonk Scraper",
+        description="A natural language processing web app that evaluates sentiment of Elon's twitter feed to determine if stonk will go up or down."
+    )
+
+@app.route('/live', methods=['GET', 'POST', 'PUT'])
+def live():
+    """Live page."""
+    nav = [
+        {'name': 'Live', 'url': 'live.html'},
+        {'name': 'Demo', 'url': 'demo.html'}
+    ]
+    return render_template(
+        'live.html',
+        title="Stonk Scraper",
         description="Live scraper for Elon's twitter feed."
+    )
+
+@app.route('/demo', method=['GET', 'POST', 'PUT'])
+def demo():
+    """Demo page."""
+    nav = [
+        {'name': 'Live', 'url': 'live.html'},
+        {'name': 'Demo', 'url': 'demo.html'}
+    ]
+    return render_template(
+        'demo.html',
+        title="Stonk Scraper",
+        description="Demo scraper for Elon's twitter feed."
     )
 
