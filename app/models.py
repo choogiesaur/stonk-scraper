5a40900741b41ed9e5bb0e850f556db6342277a4
Matthew Henderson <info@matthewahenderson.com>
2022-04-22 09:22:13 -0400

    Added results schema to models.py

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ import flask_sqlalchemy
 db = flask_sqlalchemy.SQLAlchemy()
 
 class Stonks(db.Model):
-    __tablename__ = 'stonk'
+    __tablename__ = 'stonks'
     id = db.Column(db.Integer, primary_key=True)
     ticker = db.Column(db.String(50), index=True, unique=True, nullable=False)
     stonk = db.Column(db.String(200), index=False, unique=True, nullable=False)
@@ -13,4 +13,17 @@ class Stonks(db.Model):
         self.stonk = stonk
 
     def __repr__(self):
-        return '<Ticker {}>'.format(self.ticker)
+        return '<Ticker {}, Stonk {}>'.format(self.ticker, self.stonk)
+
+class Results(db.Model):
+    __tablename__ = 'results'
+    id = db.Column(db.Integer, primary_key = True)
+    tweet = db.Column(db.String(140), index=True, unique=True, nullable=False)
+    sentiment = db.Column(db.String(30), index=True, unique=True, nullable=False)
+
+    def __init__(self, tweet, sentiment):
+        self.tweet = tweet
+        self.sentiment = sentiment
+
+    def __repr__(self):
+        return '<Tweet {}, Sentiment {}>'.format(self.tweet, self.sentiment)
